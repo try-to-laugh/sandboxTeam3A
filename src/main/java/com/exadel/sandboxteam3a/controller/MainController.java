46d663c95620cec7eaeb1f0afcb77c829a8fd8ae
AlekseiGustyr <54954723+AlekseiGustyr@users.noreply.github.com>
2022-07-07 22:34:56 +0300

    Add dao layer

--- src/main/java/com/exadel/sandboxteam3a/controller/MainController.java
+++ src/main/java/com/exadel/sandboxteam3a/controller/MainController.java
@@ -1,15 +1,33 @@
 package com.exadel.sandboxteam3a.controller;
 
+import com.exadel.sandboxteam3a.dao.entity.User;
+import com.exadel.sandboxteam3a.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController(value = "/budget")
 public class MainController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService=userService;
+    }
+    
 
     @GetMapping("/hello")
     public String sayHello() {
         return "hello world";
     }
+    
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.findAllUsers();
+    }  
 
 }
