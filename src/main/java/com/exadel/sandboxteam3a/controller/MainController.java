8e80898b4dd1f900b2d62627a80e47901f32b470
AlekseiGustyr <54954723+AlekseiGustyr@users.noreply.github.com>
2022-07-07 23:22:59 +0300

    Fix some mapping issues

--- src/main/java/com/exadel/sandboxteam3a/controller/MainController.java
+++ src/main/java/com/exadel/sandboxteam3a/controller/MainController.java
@@ -5,11 +5,13 @@ import com.exadel.sandboxteam3a.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController(value = "/budget")
+@RestController
+@RequestMapping("/budget")
 public class MainController {
 
     private UserService userService;
