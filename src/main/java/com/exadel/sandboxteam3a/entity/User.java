46f63c3a3fafcef84750cded74ab82189a488050
Nataly <nata.tereshkova@gmail.com>
2022-07-07 19:21:00 +0200

    add user entity

---
+++ src/main/java/com/exadel/sandboxteam3a/entity/User.java
@@ -0,0 +1,31 @@
+package com.exadel.sandboxteam3a.entity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id_user")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String mail;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+}
