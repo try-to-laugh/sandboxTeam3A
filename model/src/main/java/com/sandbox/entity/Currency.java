f88361f14d0e469130604415609394916b57da48
Anna Bulanchuk <anabulancuk@gmail.com>
2022-08-02 12:30:08 +0300

    added annotations to currency

--- model/src/main/java/com/sandbox/entity/Currency.java
+++ model/src/main/java/com/sandbox/entity/Currency.java
@@ -1,16 +1,13 @@
 package com.sandbox.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Currency {
     USD("USD"),
     EURO("EURO");
 
     private final String currency;
-
-    Currency(String currency) {
-        this.currency = currency;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
 }
