8e197e1a4221f13ff4013b24f898f2a4f44431c2
FLAPJVCK <vaskevich1199@mail.ru>
2022-08-03 17:37:49 +0300

    code refactor

--- model/src/main/java/com/sandbox/entity/Role.java
+++ model/src/main/java/com/sandbox/entity/Role.java
@@ -1,10 +1,8 @@
-package com.sandbox.entities;
+package com.sandbox.entity;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,13 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
 public class Role implements Serializable {
 
@@ -29,17 +25,4 @@ public class Role implements Serializable {
     private Long id;
 
     private String name;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Role role = (Role) o;
-        return id != null && Objects.equals(id, role.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
--- model/src/main/java/com/sandbox/entity/User.java
+++ model/src/main/java/com/sandbox/entity/User.java
@@ -1,10 +1,8 @@
-package com.sandbox.entities;
+package com.sandbox.entity;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.Hibernate;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -19,14 +17,12 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
 public class User implements Serializable {
 
@@ -54,17 +50,4 @@ public class User implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles = new HashSet<>();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        User user = (User) o;
-        return id != null && Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
