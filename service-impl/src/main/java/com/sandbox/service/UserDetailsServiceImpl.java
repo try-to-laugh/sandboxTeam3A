a133600b41edcb76b83b1c943cc0e2e1c09b6038
FLAPJVCK <vaskevich1199@mail.ru>
2022-08-05 02:29:38 +0300

    refactor project structure

--- service-impl/src/main/java/com/sandbox/service/UserDetailsServiceImpl.java
+++ service-impl/src/main/java/com/sandbox/service/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.sandbox.service;
 
 import com.sandbox.dto.UserDto;
-import com.sandbox.mapper.UserMapper;
 import com.sandbox.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserService userService;
-    private final UserMapper userMapper;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
